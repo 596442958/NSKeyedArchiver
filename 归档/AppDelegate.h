be7f290833186b857e361a482b2e61bd44d5fe6b
Miles <596442958@qq.com>
2016-02-04 04:48:40 +0800

    Initial Commit

---
+++ 归档/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  归档
+//
+//  Created by Miles on 2/4/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
